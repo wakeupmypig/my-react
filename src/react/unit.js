909c3863c3fc1c41f748301da3891183f5a4732b
fandonglai <704072802@qq.com>
2019-04-24 16:26:13 +0800

    react-element

--- src/react/unit.js
+++ src/react/unit.js
@@ -1,3 +1,4 @@
+import $ from 'jquery'
 class Unit{
     constructor(element){
         // 通过父类来存储element元素
@@ -16,11 +17,16 @@ class ReactNativeUnit extends Unit{
         this.rootId = rootId;
         // react源码 用的是字符串拼接
         let {props,type} = this._currentElement;
+        // 将jsx元素 转化成html字符串 domdiff
         let tagStart = `<${type} data-reactid="${rootId}"`;
         let contentString = '';
         let tagEnd = `</${type}>`;
         for(let keyProp in props){
-            if(keyProp === 'children'){
+            if(/on[A-Z]/.test(keyProp)){ // 说明现在绑定的是事件
+                // 事件委托 ele.srcElement
+                let eventType = keyProp.slice(2).toLowerCase();
+                $(document).on(eventType,`[data-reactid="${rootId}"]`,props[keyProp]);
+            }else if(keyProp === 'children'){
                 // child 可能是 字符串  可能是一个对象
                 contentString = props[keyProp].map((child,idx)=>{
                     // 递归的获取 儿子对应的字符串结果
@@ -28,7 +34,7 @@ class ReactNativeUnit extends Unit{
                     return childInstance.getMarkUp(`${rootId}.${idx}`);
                 }).join('');
             }else{
-                tagStart += (` ${keyProp}=${props[keyProp]}`)
+                tagStart += (` ${keyProp}=${props[keyProp]}`);
             }
         }
         return tagStart + '>' + contentString + tagEnd;
